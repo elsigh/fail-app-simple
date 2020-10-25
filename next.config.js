95c06cc86330fdb83ead6e1773e6bda9077e3cf4
elsigh <elsigh@gmail.com>
2020-10-24 22:10:07 -0700

    try monkeying with env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const {
+  NEXT_PUBLIC_SENTRY_DSN,
+  NEXT_PUBLIC_SENTRY_RELEASE,
+} = process.env
+
+process.env.SENTRY_DSN = NEXT_PUBLIC_SENTRY_DSN
+process.env.SENTRY_RELEASE = NEXT_PUBLIC_SENTRY_RELEASE
 
 module.exports = {
-  experimental: { plugins: true }
+  experimental: { plugins: true },
+  env: {
+    // Required for now because the experimental plugins API is not checking
+    // for variables with the NEXT_PUBLIC prefix that are automatically pulled
+    // in
+    NEXT_PUBLIC_SENTRY_DSN: NEXT_PUBLIC_SENTRY_DSN,
+  },
 }
