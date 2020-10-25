60b09b7102fb961b5c0bbba193dae6f1bb0c8e57
elsigh <elsigh@gmail.com>
2020-10-24 22:41:10 -0700

    try to get an error in client test 6 by throwing late

--- pages/client/test6.js
+++ pages/client/test6.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default class Test6 extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      error: false
+    }
+  }
+  componentDidMount() {
+    window.setTimeout(() => this.setState({ error: true }))
+  }
   render() {
-    // breaks the build
-    // throw new Error("Client Test 6")
+    if (this.state.error) {
+      throw new Error("Client Test 6")
+    }
     return (
       <div>
         <h1>Client Test 6</h1>
